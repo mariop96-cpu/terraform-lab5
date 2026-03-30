6a77bf908a9726aa0ccd94d67229ab780ea54f8e
Mario <mariop96@gmail.com>
2026-03-30 20:38:34 +0100

    fixing backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg_lab"
     storage_account_name = "fmacademystorage1234"
     container_name       = "tfstate"
-    key = "student-mario.tfstate"
+    key                  = "student-mario.tfstate"
   }
 }
